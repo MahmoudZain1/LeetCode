023fb916912d7067b3bf2d6a44fb6f0c67307057
Mahmoud Zain <zainmahmoud719@gmail.com>
2024-07-13 12:21:09 +0300

    Solved LeetCode problem.

---
+++ Solution3.java
@@ -0,0 +1,12 @@
+
+public class Solution3 {
+    public void reverse(int []numbers,int start , int end){
+        while(start < end){
+        int temp = numbers[start];
+         numbers[start]=numbers[end];
+         numbers[end] = temp;
+         start++;
+         end--;
+        }
+    }
+}
